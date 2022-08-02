7db6e874ec51e6bd1945a87b91647ab1ea237c66
Anitaegede <debbyat2008@gmail.com>
2022-08-02 11:24:40 +0100

    a

---
+++ function.js
@@ -0,0 +1,9 @@
+let twoRaiseTo2 = Math.pow(2,2)
+console.log("2 raise to the power of 2 is:",twoRaiseTo2)
+let twoRaiseTo3=Math.pow(2,3)
+console.log("2 raise to the power of 3 is:", twoRaiseTo3)
+let sixRaiseTo6 =Math.pow(6,2)
+console.log("6 raise to the power of 2 is:", sixRaiseTo6)
+function square(){
+    console.log(2 ** 2)
+}
