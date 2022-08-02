9631b052dd12995d62bee9f90be88fc3e34dcbb5
Anitaegede <debbyat2008@gmail.com>
2022-08-02 12:05:30 +0100

    done

--- function.js
+++ function.js
@@ -7,3 +7,42 @@ console.log("6 raise to the power of 2 is:", sixRaiseTo6)
 function square(){
     console.log(2 ** 2)
 }
+for(let count =1; count <11; count++){
+    square()
+}
+function printName(){
+    console.log("Egede","Anita")
+}
+printName()
+function multiply(){
+    let number1 =2
+    let number2 =4
+    let product = number1*number2
+    let message = "2 times 4 is :"+product
+    console.log(message)
+}
+multiply()
+function addition(){
+    let x =3
+    let y =9
+    let add = x+y
+    let m = "3 plus 9 is :"+add
+    console.log(m)
+}
+addition()
+function subtrction(){
+    let a =7
+    let b =4
+    let sub = a-b
+    let me = "7 minus 4 is :"+sub
+    console.log(me)
+}
+subtrction()
+function division(){
+    let g =20
+    let h =5
+    let div=g/h
+    let mes ="20 divided by 5 is :"+div
+    console.log(mes)
+}
+division()
